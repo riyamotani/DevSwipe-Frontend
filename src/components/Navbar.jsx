65103e7ac7bb1ee21bf5379847b55c5af0a39ba9
riyamotani <riyamotani601@gmail.com>
2025-06-11 21:41:10 +0530

    added logout

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,36 @@
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { removeUser } from "../utils/userSlice";
 
 const Navbar = () => {
   const user = useSelector((store) => store.user);
+  const baseUrl = import.meta.env.VITE_BASE_URL;
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await axios.post(
+        `${baseUrl}/logout`,
+        {},
+        {
+          withCredentials: true,
+        }
+      );
+      dispatch(removeUser());
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">👩‍💻DevSwipe👨‍💻</Link>
+        <Link to="/" className="btn btn-ghost text-xl">
+          👩‍💻DevSwipe👨‍💻
+        </Link>
       </div>
       {user && (
         <div className="flex gap-2">
@@ -29,7 +53,9 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <Link to="/profile" className="justify-between">Profile</Link>
+                <Link to="/profile" className="justify-between">
+                  Profile
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
