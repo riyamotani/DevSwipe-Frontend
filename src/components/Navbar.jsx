1849a2d9f6fd09a97dd7c12c19ceed313806d344
riyamotani <riyamotani601@gmail.com>
2025-06-12 23:44:55 +0530

    minor fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -43,8 +43,8 @@ const Navbar = () => {
             >
               <div className="w-10 rounded-full">
                 <img
-                  alt="Tailwind CSS Navbar component"
-                  src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                  alt="photo"
+                  src={user.photoUrl}
                 />
               </div>
             </div>
