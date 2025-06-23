685951c5ff8dc32cb68374a051f377b6a9e3a17e
riyamotani <riyamotani601@gmail.com>
2025-06-23 20:12:21 +0530

    minor change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <Link to="/" className="btn btn-ghost text-xl">
+        <Link to={!user ? "/login": "/"} className="btn btn-ghost text-xl">
           👩‍💻DevSwipe👨‍💻
         </Link>
       </div>
